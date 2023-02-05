04e8d1958d48dd9c6679b042f7b31e83bdc74ba3
Sushovit204 <shahsushovit434@gmail.com>
2023-02-05 17:11:23 +0545

    Tidy up the code making different file for database manipulation and keygen

--- shortener_app/main.py
+++ shortener_app/main.py
@@ -1,10 +1,9 @@
 import validators
-import secrets
 from fastapi import FastAPI, HTTPException, Depends, Request
 from fastapi.responses import RedirectResponse
 from sqlalchemy.orm import Session
 
-from .import schemas, models
+from .import schemas, models, crud
 from .database import SessionLocal, engine
 
 app=FastAPI()
@@ -28,38 +27,24 @@ def raise_not_found(request):
 def read_root():
     return "Welcome to fast api!"
 
+@app.post("/url", response_model=schemas.URLInfo)
+def create_url(url: schemas.URLBase, db:Session = Depends(get_db)):
+    if not validators.url(url.target_url):
+        raise_bad_request(message="Your provided url isnot valid")
+    
+    db_url = crud.create_db_url(db=db, url=url)
+    db_url.url= db_url.key
+    db_url.admin_url = db_url.secret_key
+
+    return db_url
+
 @app.get("/{url_key}")
 def foward_to_target_url(
     url_key: str,
     request: Request,
     db: Session = Depends(get_db)
     ):
-    db_url=(
-        db.query(models.URL)
-        .filter(models.URL.key == url_key, models.URL.is_active)
-        .first()
-    )
-    if db_url:
+    if db_url:= crud.get_db_url_by_key(db=db, url_key=url_key):
         return RedirectResponse(db_url.target_url)
     else:
         raise_not_found(request)
-
-
-@app.post("/url", response_model=schemas.URLInfo)
-def create_url(url: schemas.URLBase, db:Session = Depends(get_db)):
-    if not validators.url(url.target_url):
-        raise_bad_request(message="Your provided url isnot valid")
-    
-    chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    key = "".join(secrets.choice(chars) for _ in range(5))
-    secret_key = "".join(secrets.choice(chars) for _ in range(8))
-    db_url = models.URL(
-        target_url=url.target_url, key=key, secret_key = secret_key
-    )
-    db.add(db_url)
-    db.commit()
-    db.refresh(db_url)
-    db_url.url= key
-    db_url.admin_url = secret_key
-
-    return db_url
